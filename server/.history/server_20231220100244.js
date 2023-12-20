1ce16952ec422541993310a5fe5c5e6f9d0ec1cd
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2023-12-20 10:07:28 +0700

    Delete .history foler in Server folder

--- server/.history/server_20231220100244.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server is listening on port " + port);
-});
